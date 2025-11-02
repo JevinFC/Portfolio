02cb253107ab02cb5316fa345471f59877732ab4
JevinFC <k.machado06@hotmail.com>
2025-11-02 14:55:39 +0100

    Add framer-motion for animations in APropos component; update profile image styling

--- src/components/aPropos/apropos.jsx
+++ src/components/aPropos/apropos.jsx
@@ -1,5 +1,6 @@
 import './apropos.scss';
 import {useLanguage} from '../languageContext';
+import {motion} from 'framer-motion';
 function APropos() {
     const { t } = useLanguage();
 
@@ -7,20 +8,30 @@ function APropos() {
         <section className="aproposContainer" id='apropos'>
             <div className="presentationSection">
                 <h2 className='aproposh2'>{t("aboutTitle")}</h2>
-                <div className='container-info'>
-                    <p className='p-propos'>
+                 <div className="container-info">
+          {/* Texte animé */}
+          <motion.p
+            className="p-propos"
+            initial={{ opacity: 0, y: 50 }}          // position de départ
+            whileInView={{ opacity: 1, y: 0 }}       // position finale
+            transition={{ duration: 0.8, ease: "easeOut" }}
+            viewport={{ once: false, amount: 0.3 }}  // déclenche l'animation quand 30% de l'élément est visible
+          >
                         {t("aboutText1")}
-                    </p>
+            </motion.p>
             
 
                 <div className="stats-section">
                     <div className="profile-image-container">
-                        <img 
-                            src="/photoProfil.png"  //Changer photo de profil ici à celle sans fond
-                            alt="Photo de profil"
-                            className="profile-image"
-                            title='Photo de profil'
-                        />
+                        <motion.img
+            src='/photoProfil.png'
+            alt="Photo de profil"
+            className="profile-image"
+            initial={{ opacity: 0, x: 100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, ease: "easeOut" }}
+            viewport={{ once: false, amount: 0.3 }}
+          />
                     </div>
                 </div>
                 </div>
